4cad4ac6366ad011d8c90613c7b8548eb3f5ec23
21565-Alessandra <71334857+21565-Alessandra@users.noreply.github.com>
2020-11-17 15:32:52 +0000

    Update Panel.java

--- src/com/dorsetcollegeoop/Panel.java
+++ src/com/dorsetcollegeoop/Panel.java
@@ -1,15 +1,12 @@
-package com.dorsetcollegeoop;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class Panel {
-    //cada panel tem 50 números
-    //in the code below I created a list in java with 50 numbers, to guarantee that the panel will have 50 numbers.
+    //each panel has 50 numbers
+    //in the code below we created a list in java with 50 numbers, so we guarantee that the panel will have 50 numbers.
     private List<Integer> numbers = new ArrayList<Integer>(50);
-    //List<Integer> myCoords = new ArrayList<Integer>();
 
-    //Here we have a constructor that receives a list of numbers
+    //Here we have a constructor that receives a list of integers
     public Panel(List<Integer> numbers) {
         //Here we scan this list of numbers to be later saved in the list
         for (int i = 0; i < numbers.size(); i++) {
@@ -18,15 +15,9 @@ public class Panel {
         }
     }
 
-    public List<Integer> getNumbers() {
-        return numbers;
-    }
-
-
     public void addNumber(int number) {
         //here we guarantee that the number is not less than 0 nor greater than 50
         if(number >= 0 && number <= 50)
             this.numbers.add(number);
     }
 }
-
