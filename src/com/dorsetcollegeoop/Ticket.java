038ceb6d20352355c5680bb8ab0c3cfaee4d87ff
21565-Alessandra <71334857+21565-Alessandra@users.noreply.github.com>
2020-11-17 15:31:18 +0000

    Update Ticket.java

--- src/com/dorsetcollegeoop/Ticket.java
+++ src/com/dorsetcollegeoop/Ticket.java
@@ -1,30 +1,55 @@
-package com.dorsetcollegeoop;
-
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 public class Ticket {
-    //ticket tem 8 painéis
+    //ticket has 8 panels
     private List<Panel> panels = new ArrayList<Panel>(8);
 
-    public Ticket() { }
+    public Ticket() {}
 
-    public Ticket(List<Panel> panels) {
-        this.panels = panels;
-    }
-
-    public List<Panel> getPanels() {
-        for(int i = 0; i < panels.size(); i++){
-            panels.get(i).getNumbers();
+    //here is the method that calculates the total bet amount
+    //for each panel is added 2 euros
+    public double calculateCostLotteryPurchase(){
+        double valuePanel = 2.00;
+        double total = 0.00;
+        for (int i = 0; i < panels.size(); i ++){
+            total = total + valuePanel;
         }
-        return panels;
+        return total;
     }
 
-    public void setPanels(List<Panel> panels) {
-        this.panels = panels;
+    public void addPanels(Boolean generateNumber, List<Integer> numbersUser){
+        //Here was created a list of integers with 6 numbers, numbers that
+        //are the maximum amount that we can register in a game
+        List<Integer> numbers = new ArrayList<Integer>(6);
+        //Here we check if the user wants 6 RANDOM numbers to be registered
+        //if you want, we call the method that generates a list of random numbers, otherwise
+        //we take the user's number.
+        if(generateNumber == true){
+            numbers = generateRandomNumbers();
+        }else{
+            numbers = numbersUser;
+        }
+        //Aqui estamos dizendo que para cada painel (no total de 8) estamos adicionando
+        //a list of 6 numbers
+        for(Panel panel:panels){
+            this.panels.add(new Panel(numbers));
+        }
     }
 
-    public void addPanels(Panel panel){
-        this.panels.add(panel);
+    //here this is the method that generates 6 random numbers
+    public List<Integer> generateRandomNumbers(){
+        Random rand = new Random();
+        //generates random numbers from 0 to 50
+        int n1 = rand.nextInt(51);
+        int n2 = rand.nextInt(51);
+        int n3 = rand.nextInt(51);
+        int n4 = rand.nextInt(51);
+        int n5 = rand.nextInt(51);
+        int n6 = rand.nextInt(51);
+        return Arrays.asList(n1, n2, n3, n4, n5, n6);
     }
 }
